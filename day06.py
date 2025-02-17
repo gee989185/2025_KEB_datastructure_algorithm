7cae7a056b2e89c537e932c37c2f4a6fd0996ee7
gee989185 <gee020915@gmail.com>
2025-02-17 11:35:47 +0900

    v0.3 day06.py O(n)

--- day06.py
+++ day06.py
@@ -1,38 +1,10 @@
-# # 8진수 만들기
-# def oct(number) -> int:
-#     if number // 8 > 8:
-#         result +=1
-#
-#
-#         7 > 7
-#         10 > 8
-#         11 > 9
-#         64 > 101
-#
-#
-# # 2진수 만들기
-# def bin(number):
-#     2 > 10 (2 // 2 = 1)
-#     3 > 11
-#     4 > 100 ( 4 // 2 = 2)
-#     6 > 110
-#     8 > 1000 (8 // 2 = 3)
-#     10 > 1010 (10 // 2 = )
-#
-def dec_bin(n):
-    if n == 0:
-        return ""
-    else:
-        return dec_bin(n // 2) + str(n % 2)
-
-def dec_oct(n) -> int:
-    if n == 0:
-        return ""
-    else:
-        return dec_oct(n // 8) + str(n % 8)  # 8로 나누면 몫
-
+# O(n) 선형시간
+def total(n):
+    result = 0
+    for i in range(n+1):
+        result += i
+    return result
 n = int(input())
-print(dec_oct(n))
+print(total(n))
 
-n1 = int(input())
-print(dec_bin(n1))
+
