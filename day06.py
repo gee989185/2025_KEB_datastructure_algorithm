b62c787e4ef183454e937ed7db21428bc5d1a4f8
gee989185 <gee020915@gmail.com>
2025-02-17 10:41:39 +0900

    v0.2 day06.py recursion_ocatal

--- day06.py
+++ day06.py
@@ -1,12 +1,38 @@
-def is_even(n) -> bool:
-    """
-    짝수 판정 함수
-    :param n: 판정할 정수
-    :return: 짝수면 True, 홀수면 False
-    """
-    if n % 2 == 0:
-        return True
-    return False
+# # 8진수 만들기
+# def oct(number) -> int:
+#     if number // 8 > 8:
+#         result +=1
+#
+#
+#         7 > 7
+#         10 > 8
+#         11 > 9
+#         64 > 101
+#
+#
+# # 2진수 만들기
+# def bin(number):
+#     2 > 10 (2 // 2 = 1)
+#     3 > 11
+#     4 > 100 ( 4 // 2 = 2)
+#     6 > 110
+#     8 > 1000 (8 // 2 = 3)
+#     10 > 1010 (10 // 2 = )
+#
+def dec_bin(n):
+    if n == 0:
+        return ""
+    else:
+        return dec_bin(n // 2) + str(n % 2)
+
+def dec_oct(n) -> int:
+    if n == 0:
+        return ""
+    else:
+        return dec_oct(n // 8) + str(n % 8)  # 8로 나누면 몫
 
 n = int(input())
-print(is_even(n))
+print(dec_oct(n))
+
+n1 = int(input())
+print(dec_bin(n1))
